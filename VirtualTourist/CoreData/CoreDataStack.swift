6d37c48493c305f0c030324c4a6a39c127b007e3
Matheus Lima <matheus.lima@lavid.ufpb.br>
2019-02-08 12:31:54 -0300

    feat: auto save support

--- VirtualTourist/CoreData/CoreDataStack.swift
+++ VirtualTourist/CoreData/CoreDataStack.swift
@@ -44,12 +44,25 @@ final class CoreDataStack {
             guard error == nil else {
                 fatalError(error!.localizedDescription)
             }
+            self.autoSaveViewContext()
             self.configureContexts()
             completion?()
         }
     }
     
     // MARK: - Core Data Saving Support
+    
+    func autoSaveViewContext(interval: TimeInterval = 30) {
+        guard interval > 0 else { print("interval must be positive"); return }
+        
+        if viewContext.hasChanges {
+            try? viewContext.save()
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + interval) {
+            self.autoSaveViewContext(interval: interval)
+        }
+    }
     
     func saveViewContext(errorHandler: ((_ error: Error?) -> Void)? = nil) {
         viewContext.perform {
